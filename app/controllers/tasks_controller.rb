996f7e7baffe4dbbe6fc48f8f15e4aca5b462134
Alex Poltorapavlov <poltorapavlov.iskander@gmail.com>
2024-05-20 09:43:36 +0300

    Broken app

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,15 +14,25 @@ class TasksController < ApplicationController
     @task = @project.tasks.build(task_params)
 
     if @task.save
-      redirect_to project_path(@project), notice: 'Task was successfully created.'
+      redirect_to project_path(@project), notice: 'Задача была успешно выполнена'
     else
       render :new
+    end
+  end
+
+  def update
+    @task = Task.find(params[:id])
+
+    if @task.update(task_params)
+      redirect_to @task
+    else
+      render :edit
     end
   end
 
   private
 
   def task_params
-    params.require(:task).permit(:title, :body)
+    params.require(:task).permit(:title, :body, :duration)
   end
 end
