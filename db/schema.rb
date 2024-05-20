b25576cf5d877888067dd5aadf40756dbf9b91a3
Alex Poltorapavlov <poltorapavlov.iskander@gmail.com>
2024-05-20 06:22:10 +0300

    Added default value for doration of task

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_20_015226) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_20_032037) do
   create_table "projects", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -18,6 +18,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_20_015226) do
     t.datetime "updated_at", null: false
     t.integer "user_id", null: false
     t.index ["user_id"], name: "index_projects_on_user_id"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "duration", default: 0
+    t.integer "project_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_tasks_on_project_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -36,4 +46,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_20_015226) do
   end
 
   add_foreign_key "projects", "users"
+  add_foreign_key "tasks", "projects"
 end
