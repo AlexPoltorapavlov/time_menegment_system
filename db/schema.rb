6fa3b2c798b3d9f42e9469b1ad99667a23f8f67b
Alex Poltorapavlov <poltorapavlov.iskander@gmail.com>
2024-05-24 17:33:55 +0300

    Edited tasks structure. Added total_time and last_started_at

--- db/schema.rb
+++ db/schema.rb
@@ -26,9 +26,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_20_031517) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.datetime "duration"
-    t.datetime "start_time"
-    t.datetime "stop_time"
+    t.integer "total_time", default: 0, null: false
+    t.datetime "last_started_at"
     t.bigint "project_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
