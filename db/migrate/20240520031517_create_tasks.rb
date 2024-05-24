ae2a106225ad67583ead54c245f0c39dc3f83102
Alex Poltorapavlov <poltorapavlov.iskander@gmail.com>
2024-05-24 18:14:09 +0300

    Added table 'timers'

--- db/migrate/20240520031517_create_tasks.rb
+++ db/migrate/20240520031517_create_tasks.rb
@@ -3,8 +3,6 @@ class CreateTasks < ActiveRecord::Migration[7.1]
     create_table :tasks do |t|
       t.string :title
       t.text :body
-      t.integer :total_time, default: 0, null: false
-      t.datetime :last_started_at
       t.references :project, null: false, foreign_key: true
 
       t.timestamps
