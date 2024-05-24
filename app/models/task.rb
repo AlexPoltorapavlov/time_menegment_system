15a7bb45b0c6f17bcdb8b7d9e94318b829756d4c
Alex Poltorapavlov <poltorapavlov.iskander@gmail.com>
2024-05-24 18:19:26 +0300

    Added new model 'timer'

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :project
+  has_many :timers
   validates :title, presence: true, length: {minimum: 5}
   validates :body, presence: true, length: {minimum: 10}
 end
