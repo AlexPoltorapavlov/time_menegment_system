44296ba13d836e1e9b0a630fcb39ac88af1556ef
Alex Poltorapavlov <poltorapavlov.iskander@gmail.com>
2024-05-25 18:47:08 +0300

    Edited migration 'create_timers'

--- db/migrate/20240524144918_create_timers.rb
+++ db/migrate/20240524144918_create_timers.rb
@@ -2,6 +2,7 @@ class CreateTimers < ActiveRecord::Migration[7.1]
   def change
     create_table :timers do |t|
       t.datetime :started_at
+      t.datetime :stoped_at
       t.integer :total_time, default: 0, null: false
       t.references :task, null: false, foreign_key: true
       t.datetime :date_of_timer
