08e8e807566a0baa048c81c470b46fe74b6633a2
Alex Poltorapavlov <poltorapavlov.iskander@gmail.com>
2024-05-28 11:05:08 +0300

    Added more space to code

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,7 @@
 class Project < ApplicationRecord
   belongs_to :user
   has_many :tasks, dependent: :destroy
+
   validates :title, presence: true, length: { minimum: 5 }
   validates :body, presence: true, length: { minimum: 5 }
 end
