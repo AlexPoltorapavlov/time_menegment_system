ee55fe9c8755f5c1e9f6ccd86023334fdf0d80af
Alex Poltorapavlov <poltorapavlov.iskander@gmail.com>
2024-06-18 10:02:59 +0300

    Setting for sending mails

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,17 @@
 class ApplicationController < ActionController::Base
   # helper_method :current_user
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :phonenumber])
+  end 
 
   private
-
-  # def current_user
-  #   @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-  # end
 
   def authenticate_user!
     redirect_to root_path, alert: 'Пожалуйста, войдите в систему.' unless current_user
   end
+
 end
