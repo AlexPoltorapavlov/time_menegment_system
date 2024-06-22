93ee82550a9009eefdde242474f53f57a2cd18c9
Alex Poltorapavlov <poltorapavlov.iskander@gmail.com>
2024-06-22 15:20:55 +0300

    Removed duplicates in decorators

--- app/decorators/task_decorator.rb
+++ app/decorators/task_decorator.rb
@@ -3,9 +3,6 @@ class TaskDecorator < Draper::Decorator
 
   def formatted_total_time
     total_time = object.timers.sum(:total_time)
-    hours = (total_time / 3600).to_i
-    minutes = ((total_time % 3600) / 60).to_i
-    seconds = (total_time % 60).to_i
-    format('%<hours>02d:%<minutes>02d:%<seconds>02d', hours: hours, minutes: minutes, seconds: seconds)
+    helpers.formatted_time(total_time)
   end
 end
--- app/decorators/timer_decorator.rb
+++ app/decorators/timer_decorator.rb
@@ -2,9 +2,6 @@ class TimerDecorator < Draper::Decorator
   delegate_all
 
   def formatted_time
-    hours = (total_time / 3600).to_i
-    minutes = ((total_time % 3600) / 60).to_i
-    seconds = (total_time % 60).to_i
-    format('%<hours>02d:%<minutes>02d:%<seconds>02d', hours: hours, minutes: minutes, seconds: seconds)
+    helpers.formatted_time(total_time)
   end
 end
