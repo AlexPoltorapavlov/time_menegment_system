4a6e8edd735a5bf49bad95463677ec4c51818f0d
Alex Poltorapavlov <poltorapavlov.iskander@gmail.com>
2024-07-06 12:48:03 +0300

    Turbo works without the need for modification

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,11 +1,1 @@
 //= require @hotwired/turbo-rails
-
-isTurbolinksEnabled = () => {
-  try {
-    return Turbolinks.supported;
-  } catch(_) {
-    return false;
-  }
-}
-
-console.log(isTurbolinksEnabled());
